99fec847045d15b51d7f1b86ee57f396b11629c0
AlanGO <alan.guerrao@hotmail.com>
2025-06-01 23:26:19 -0600

    Creacion de los modelos/base de datos

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categorias, Marcas, Productos
 
 # Register your models here.
+
+admin.site.register(Categorias)
+admin.site.register(Marcas)
+admin.site.register(Productos)
--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Categorias(models.Model):
+    nombre = models.CharField(max_length=100)
+    imagen = models.ImageField(upload_to='categorias/')
+
+    def __str__(self):
+        return self.nombre
+
+
+class Marcas(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Productos(models.Model):
+    nombre = models.CharField(max_length=100)
+    imagen = models.ImageField(upload_to='productos/')
+    stock = models.PositiveIntegerField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    descripcion = models.TextField()
+    marca = models.ForeignKey(Marcas, on_delete=models.SET_NULL, null=True, blank=True)
+    categoria = models.ForeignKey(Categorias, on_delete=models.CASCADE, related_name='productos')
+
+    def __str__(self):
+        return self.nombre
